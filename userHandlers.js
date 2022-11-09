ce1a307a9f1e54688a87555dc1fa30f24bdf5686
mlgs <mathieu.langlois@voxm.live>
2022-11-09 15:45:22 +0100

    insert PUT function for movies and users

--- userHandlers.js
+++ userHandlers.js
@@ -48,8 +48,30 @@ const postUser = (req, res) => {
         });
 };
 
+const updateUser = (req, res) => {
+    const id = parseInt(req.params.id);
+    const { title, director, year, color, duration } = req.body;
+
+    database
+        .query(
+            "update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?",
+            [firstname, lastname, email, city, language, id]
+        )
+        .then(([result]) => {
+            if (result.affectedRows === 0) {
+                res.status(404).send("Not Found");
+            } else {
+                res.sendStatus(204);
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error editing the movie");
+        });
+};
+
 
 module.exports = {
   getUsers,
-  getUserById, postUser,
+  getUserById, postUser, updateUser
 };
